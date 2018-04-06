137c2bc91adedcbe3f5809cb1a68c38bfa80ea74
cassio8 <cassiohenriquecahe@gmail.com>
2018-04-06 19:46:39 -0300

    Atualizando codigo ordenacao

--- wp-content/themes/hq/single-hqs.php
+++ wp-content/themes/hq/single-hqs.php
@@ -3,5 +3,29 @@
     $data         = Timber::get_context();
     $data['post'] = Timber::get_post();
 
+	$paginas = get_field('paginas');
+
+	$context = array();
+	$i = 0;
+	foreach ($paginas as $pagina) {
+		$aux = new TimberImage($pagina['pagina']);
+		$context[$i]['file'] = $aux->file;
+		$context[$i]['id'] =  $aux->id;
+		$i = $i + 1;
+	}
+
+    #$context = new TimberImage($paginas[0]['pagina']);
+
+	#echo "<pre>";
+    #print_r($context);
+    #echo "</pre>";
+
+	sort($context); #true or false
+
+	$data['context'] = $context;
+    #echo "<pre>";
+    #print_r($context);
+    #echo "</pre>";
+
 	Timber::render('single-hqs.twig', $data);
 
